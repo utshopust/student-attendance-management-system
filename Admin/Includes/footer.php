b4465c5b466aa09e4726368f9c3d73dd97a87f93
utshopust <mcutsho96@gmail.com>
2023-07-17 11:21:09 +0600

    first commit

---
+++ Admin/Includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span style="color:#FF00FF;"> &copy; PUST CSE ATTENDANCE SYSTEM
+            </span>
+          </div>
+        </div>
+      </footer>
